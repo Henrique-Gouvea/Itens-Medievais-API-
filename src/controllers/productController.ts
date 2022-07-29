2d58c1dfd65b5ebdd29ea23601e50b9a061c2dcc
Henrique-Gouvea <henrque.bonfim2@gmail.com>
2022-07-29 15:10:47 -0300

    feat(interfaces): Criando  interface dos IProducts

--- src/controllers/productController.ts
+++ src/controllers/productController.ts
@@ -1,6 +1,6 @@
 import { Request, Response, NextFunction } from 'express';
 import * as productService from '../services/productService';
-import { IProduct } from '../interfaces/IIProduct';
+import { IProduct } from '../interfaces/IProduct';
 
 async function create(req: Request, res: Response, next: NextFunction) {
   try {
