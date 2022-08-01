8aca1c8eeaff952dc78d340ca979e85853b2fe27
Henrique-Gouvea <henrque.bonfim2@gmail.com>
2022-08-01 08:07:55 -0300

    fix(models): Corrigindo SQL para selecionar os Ids dos produtos dento do arrau

--- src/models/ordersModel.ts
+++ src/models/ordersModel.ts
@@ -2,7 +2,16 @@ import { IOrder } from '../interfaces/IOrder';
 import connection from './connection';
 
 async function getAll(): Promise<IOrder[]> {
-  const query = 'SELECT * FROM Trybesmith.Orders';
+  const query = `SELECT
+    Orders.id,
+    Orders.userId,
+    JSON_ARRAYAGG(Products.id) AS productsIds
+  FROM
+    Trybesmith.Orders
+  INNER JOIN
+    Trybesmith.Products ON Products.orderId = Orders.id
+  GROUP BY Orders.id
+  ORDER BY Orders.userId;`;
 
   const [result] = await connection.execute(query);
   console.log(result);
